21db7dae115a3bb7e76abac200f490c4fcdc4200
John Duff <jduff@johns-macbook-pro.local>
2017-05-12 20:52:26 -0400

    Initial Commit

---
+++ Pins/Pins.h
@@ -0,0 +1,19 @@
+//
+//  Pins.h
+//  Pins
+//
+//  Created by John Duff on 2017-05-12.
+//  Copyright © 2017 John Duff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Pins.
+FOUNDATION_EXPORT double PinsVersionNumber;
+
+//! Project version string for Pins.
+FOUNDATION_EXPORT const unsigned char PinsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pins/PublicHeader.h>
+
+
